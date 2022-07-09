26c0eebeffbccc49328281bb939bdd41a16c1e9c
nikita <nikita.sandoor7@gmail.com>
2022-07-09 16:53:46 +0530

    added route for contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: "", component: LandingPageComponent }
+  { path: "", component: LandingPageComponent },
+  {path:"contact",component:ContactComponent}
+
 ];
 
 @NgModule({
