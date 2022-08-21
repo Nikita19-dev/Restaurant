7ea4e455f4b09dd43f1750491a9f6b80a28af54e
nikita <nikita.sandoor7@gmail.com>
2022-08-21 16:05:57 +0530

    added menu Item

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -14,46 +14,71 @@ export class MenuComponent implements OnInit {
   addtocartvisible = false
   cartkeys: string[] = []
   emptycart = false
+  noOfItems = 0
+  cartArray: any = []
+  finalTotal: number = 0;
 
-  addtoCart(id: any) {
+  calculateTotal() {
+    let total = 0;
+    this.cartArray.forEach(
+      (cart: any) => {
+        total = total + cart.qty * cart.price;
+      }
 
-    console.log("printing id", id)
+    )
+    console.log("subtotal:", total)
+    this.finalTotal = total
+  }
+
+  addtoCart(item: any) {
+    this.noOfItems = this.noOfItems + 1;
+    console.log("printing id", item.id)
     if (this.tempcart) {
 
-      if (this.tempcart[id]) {
-        console.log("printing id in if condition ", id)
-        this.tempcart[id] = this.tempcart[id] + 1
+      if (this.tempcart[item.id]) {
+        console.log("printing id in if condition ", item)
+        this.tempcart[item.id].qty = this.tempcart[item.id].qty + 1
+
 
       }
       else {
-        console.log("printing id in sec else condition ", id)
-        this.tempcart[id] = 1
-        console.log("printing id in sec else condition ", this.tempcart[id])
+        console.log("printing id in sec else condition ", item.id)
+        this.tempcart[item.id] = { ...item, "qty": 1 }
+        console.log("printing id in sec else condition ", this.tempcart[item.id])
 
       }
       this.emptycart = false
       this.cartkeys = Object.keys(this.tempcart)
+      this.cartArray = Object.values(this.tempcart)
     }
     else {
       //which ever first value it goes it stores as key then it go on adding value
       this.tempcart = {}
-      console.log("printing id in else condition ", id)
-      this.tempcart[id] = 1
+      console.log("printing id in else condition ", item.id)
+      this.tempcart[item.id] = { ...item, "qty": 1 }
       this.cartkeys = Object.keys(this.tempcart)
+      this.cartArray = Object.values(this.tempcart)
     }
+    this.calculateTotal()
 
+    console.log(this.tempcart)
+    // console.log("printing value tempcart", this.tempcart, this.tempcart[item.id].length)
 
-    console.log("printing value tempcart", this.tempcart, this.tempcart[id].length)
   }
-  removefromCart(id: any) {
-    if (this.tempcart[id] > 0) {
-      this.tempcart[id] = this.tempcart[id] - 1
-    }
-    if (this.tempcart[id] == 0) {
-      this.emptycart = true
+  removefromCart(items: any) {
+    this.noOfItems = this.noOfItems - 1;
+    if (this.tempcart[items.id].qty > 0) {
+      this.tempcart[items.id].qty = this.tempcart[items.id].qty - 1
+
+      if (this.tempcart[items.id].qty == 0) {
+        delete this.tempcart[items.id]
+      }
     }
 
+    this.cartkeys = Object.keys(this.tempcart)
+    this.cartArray = Object.values(this.tempcart)
 
+    this.calculateTotal()
 
   }
 
@@ -63,6 +88,7 @@ export class MenuComponent implements OnInit {
 
   menu_items = [
     {
+      id: 0,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
@@ -75,42 +101,49 @@ export class MenuComponent implements OnInit {
 
     },
     {
+      id: 1,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
 
     },
     {
+      id: 2,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
 
     },
     {
+      id: 3,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
 
     },
     {
+      id: 4,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
 
     },
     {
+      id: 5,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
 
     },
     {
+      id: 6,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
 
     },
     {
+      id: 7,
       name: "Spanish Egg sandwich",
       price: 50,
       img_url: "assets/food1.jpg"
